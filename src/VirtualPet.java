742313119e80b8764d4c645236b7cf27ed3c02e2
Barb Baugher <barbbaugher@gmail.com>
2017-09-07 19:12:06 -0400

    Saving updates

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,4 +1,18 @@
 
 public class VirtualPet {
 
+	// Attributes & Initial Values
+	int hunger = 8;
+	int boredom = 5;
+	int tired = 10;
+
+	// Activities
+	
+	void letsEat (int food) {
+		hunger += food;
+	}
+	
+	void letsDance (int fun) {
+		boredom += fun;
+	}
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -1,9 +1,43 @@
+import java.util.Scanner;
 
 public class VirtualPetApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
 
+		VirtualPet theDanimal = new VirtualPet();
+
+		System.out.println("Meet the Danimal:");
+		System.out.println("The Danimal is not hungry: " + theDanimal.hunger);
+		System.out.println("The Danimal is bored: " + theDanimal.boredom);
+		System.out.println("The Danimal is not sleepy: " + theDanimal.tired);
+		System.out.println("Please select an option below by entering the corresponding number:");
+		System.out.println("1.  Let's Eat!");
+		System.out.println("2.  Let's Dance!");
+		System.out.println("3.  Let's Read a Bedtime Story!");
+		System.out.println("4.  Or you can be lame and we can do nothing...");
+
+		String menuEntry = input.next();
+
+		switch (menuEntry) {
+		case "1":
+			System.out.println("Let's Eat!");
+			int food = 2;
+			theDanimal.letsEat(food);
+			System.out.println("Nom Nom Nom" + theDanimal.hunger);
+			break;
+		case "2":
+			int fun = 5;
+			theDanimal.letsDance(fun);
+			System.out.println("Let's dance\r\n" + "Put on your red shoes and dance the blues\r\n" + "Let's dance\r\n"
+					+ "To the song they're playin' on the radio\r\n" + theDanimal.boredom);
+			break;
+			
+			
+
+		}
+
+		input.close();
 	}
 
 }
