aaaa24c5a1f922fdc1d81d8e1c37a6e9f4acfb6a
Barb Baugher <barbbaugher@gmail.com>
2017-09-07 20:20:19 -0400

    Saving updates

--- src/VirtualPet.java
+++ src/VirtualPet.java
@@ -1,22 +1,48 @@
 
 public class VirtualPet {
 
-	// Attributes & Initial Values
-	int hungerLevel = 8;
-	int boredomLevel = 5;
-	int tirednessLevel = 10;
+	// Attributes & Initial Status
+
+	int hungerLevel;
+	int boredomLevel;
+	int tirednessLevel;
+
+	public VirtualPet(int hunger, int boredom, int tiredness) {
+
+		hungerLevel = hunger;
+		boredomLevel = boredom;
+		tirednessLevel = tiredness;
+
+	}
+
+	// Creating the display menu
+
+	public String menu() {
+
+		return ("Please select an option below by entering the corresponding number:\n1.  Let's Eat!\n2.  Let's Dance! \n3.  Let's Read a Bedtime Story! \n4.  Or we can be lame and we can do nothing...");
+
+	}
+
+	// Creating the status display
+
+	public String status() {
+
+		return ("Hunger Level " + hungerLevel + "\nBoredom Level " + boredomLevel + "\nTiredness Level "
+				+ tirednessLevel);
+
+	}
 
 	// Activities
-	
-	void letsEat (int food) {
+
+	void letsEat(int food) {
 		hungerLevel += food;
 	}
-	
-	void letsDance (int fun) {
+
+	void letsDance(int fun) {
 		boredomLevel += fun;
 	}
-	
-	void letsReadABedtimeStory (int sleep) {
+
+	void letsReadABedtimeStory(int sleep) {
 		tirednessLevel += sleep;
 	}
 }
--- src/VirtualPetApp.java
+++ src/VirtualPetApp.java
@@ -5,17 +5,11 @@ public class VirtualPetApp {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 
-		VirtualPet theDanimal = new VirtualPet();
+		VirtualPet theDanimal = new VirtualPet(8, 5, 10);
 
-		System.out.println("Meet the Danimal:");
-		System.out.println("The Danimal is not hungry: " + theDanimal.hungerLevel);
-		System.out.println("The Danimal is bored: " + theDanimal.boredomLevel);
-		System.out.println("The Danimal is not sleepy: " + theDanimal.tirednessLevel);
-		System.out.println("Please select an option below by entering the corresponding number:");
-		System.out.println("1.  Let's Eat!");
-		System.out.println("2.  Let's Dance!");
-		System.out.println("3.  Let's Read a Bedtime Story!");
-		System.out.println("4.  Or you can be lame and we can do nothing...");
+		System.out.println("Meet the Danimal");
+		System.out.println(theDanimal.status());
+		System.out.println(theDanimal.menu());
 
 		String menuEntry = input.next();
 
@@ -23,18 +17,18 @@ public class VirtualPetApp {
 		case "1":
 			int food = 2;
 			theDanimal.letsEat(food);
-			System.out.println("Nom Nom Nom" + theDanimal.hungerLevel);
+			System.out.println("Nom Nom Nom");
 			break;
 		case "2":
 			int fun = 5;
 			theDanimal.letsDance(fun);
-			System.out.println("Put on your red shoes and dance the blues" + theDanimal.boredomLevel);
+			System.out.println("Put on your red shoes and dance the blues");
 			break;
 
 		case "3":
 			int sleep = 5;
 			theDanimal.letsReadABedtimeStory(sleep);
-			System.out.println("Once upon a time... " + theDanimal.tirednessLevel);
+			System.out.println("Once upon a time... ");
 			break;
 
 		case "4":
